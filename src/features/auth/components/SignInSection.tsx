c675cc55695ffe87c1f7004bfc6d4f1291f5d46d
jackcasper <jackcasp69@gmail.com>
2025-04-30 22:09:58 +0100

    Full project update: rewrote components and structure

--- src/features/auth/components/SignInSection.tsx
+++ src/features/auth/components/SignInSection.tsx
@@ -12,6 +12,7 @@ import { FcGoogle } from "react-icons/fc"
 import { SignInFlow } from "../types";
 import { useState } from "react";
 import { useAuthActions } from "@convex-dev/auth/react";
+import { TriangleAlert } from 'lucide-react';
 
 interface SignInSectionProps {
     setState: (state: SignInFlow) => void;
@@ -23,6 +24,20 @@ export const SignInSection = ({ setState }: SignInSectionProps) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [pending, setPending] = useState(false);
+    const [error, setError] = useState("");
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setPending(true);
+        signIn("password", { email, password, flow: "signIn" })
+            .catch(() => {
+                setError("Invalid Email or Password");
+            })
+            .finally(() => {
+                setPending(false);
+            });
+    };
 
     const onProviderSignIn = (value: "google") => {
         setPending(true);
@@ -42,8 +57,14 @@ export const SignInSection = ({ setState }: SignInSectionProps) => {
                     Enter your Email or use another service
                 </CardDescription>
             </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className="size-4" />
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form onSubmit={onPasswordSignIn} className="space-y-2.5">
                     <Input
                         disabled={pending}
                         value={email}
--- src/features/auth/components/SignUpSection.tsx
+++ src/features/auth/components/SignUpSection.tsx
@@ -11,15 +11,48 @@ import { Separator } from "@/components/ui/separator";
 import { FcGoogle } from "react-icons/fc"
 import { SignInFlow } from "../types";
 import { useState } from "react";
+import { TriangleAlert } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignUpSectionProps {
     setState: (state: SignInFlow) => void;
 };
 
 export const SignUpSection = ({ setState }: SignUpSectionProps) => {
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [pending, setPending] = useState(false);
+    const [error, setError] = useState("");
+
+    const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("Your passwords don't match. Please try again.");
+            return;
+        }
+
+        setPending(true);
+        signIn("password", { email, password, flow: "signUp" })
+            .catch(() => {
+                setError("An unexpected error occurred. Please try again.");
+            })
+            .finally(() => {
+                setPending(false);
+            })
+    };
+
+    const onProviderSignUp = (value: "google") => {
+        setPending(true);
+        signIn(value)
+            .finally(() => {
+                setPending(false);
+        })
+    };
+
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
@@ -30,10 +63,16 @@ export const SignUpSection = ({ setState }: SignUpSectionProps) => {
                     Enter your Email or use another service
                 </CardDescription>
             </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className="size-4" />
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form onSubmit={onPasswordSignUp} className="space-y-2.5">
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="Email"
@@ -41,7 +80,7 @@ export const SignUpSection = ({ setState }: SignUpSectionProps) => {
                         required
                     />
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="Password"
@@ -49,22 +88,22 @@ export const SignUpSection = ({ setState }: SignUpSectionProps) => {
                         required
                     />
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                         placeholder="Confirm password"
                         type="password"
                         required
                     />
-                    <Button type="submit" className="w-full" size="lg" disabled={false}>
+                    <Button type="submit" className="w-full" size="lg" disabled={pending}>
                         Continue
                     </Button>
                 </form>
                 <Separator />
                 <div className="flex flex-col gap-2.5">
                     <Button
-                        disabled={false}
-                        onClick={() => { }}
+                        disabled={pending}
+                        onClick={() => onProviderSignUp("google")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
