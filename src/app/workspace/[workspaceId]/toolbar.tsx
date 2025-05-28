53e2f4a3f4b4158f471b327bb5992af68df33133
jackcasper <jackcasp69@gmail.com>
2025-05-28 21:45:04 +0100

    fix build errors

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -57,7 +57,7 @@ export const Toolbar = () => {
                         <CommandEmpty>No results found.</CommandEmpty>
                         <CommandGroup heading="Channels">
                             {channels?.map((channel) => (
-                                <CommandItem onSelect={() => onChannelClick(channel._id)}>
+                                <CommandItem key={channel._id} onSelect={() => onChannelClick(channel._id)}>
                                     {channel.name}
                                 </CommandItem>
                             ))}
@@ -65,7 +65,7 @@ export const Toolbar = () => {
                         <CommandSeparator />
                         <CommandGroup heading="Members">
                             {members?.map((member) => (
-                                <CommandItem onSelect={() => onMemberClick(member._id)}>
+                                <CommandItem key={member._id} onSelect={() => onMemberClick(member._id)}>
                                     {member.user.name}
                                 </CommandItem>
                             ))}
